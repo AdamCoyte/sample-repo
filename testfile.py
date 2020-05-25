9c03294aa0e14fd156bc0f9375586ad67abbd3cb
AdamCoyte <adam.coyte00@gmail.com>
2020-05-25 08:51:32 -0400

    remove some code

--- testfile.py
+++ testfile.py
@@ -1,21 +1,6 @@
 
-import math
-from os import rename
-import sys
+import requests
  
-# import requests
-
-# print("--",sys.version)
-# print("--",sys.executable)
-
-
-# def greet(who_to_greet):
-#     greeting = 'Hello, {}'.format(who_to_greet)
-#     return greeting
-
-# print(greet("Adam"))
-# r = requests.get('https://www.google.com')
-# print(r.status_code)
-
-name = input("Your name?")
-print("Hello,", name)
+r = requests.get('https://www.google.com')
+print(r.status_code)
+print(r.ok)
