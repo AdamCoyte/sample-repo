85ed8de4e6ef93ea52e20f302e8fa2471de76447
Adam Coyte <adam.coyte@LAP-00200.local>
2020-05-25 08:43:15 -0400

    Initial Commit

---
+++ testfile.py
@@ -0,0 +1,21 @@
+
+import math
+from os import rename
+import sys
+ 
+# import requests
+
+# print("--",sys.version)
+# print("--",sys.executable)
+
+
+# def greet(who_to_greet):
+#     greeting = 'Hello, {}'.format(who_to_greet)
+#     return greeting
+
+# print(greet("Adam"))
+# r = requests.get('https://www.google.com')
+# print(r.status_code)
+
+name = input("Your name?")
+print("Hello,", name)
